76b21fdf2a68a66f7cd3b72929286cf5c4799c9d
johndcusack <mrjdcusack@gmail.com>
2026-07-06 16:00:25 +0100

    removes unused module, imports logging

--- 0_raw_to_bronze/test_clean.py
+++ 0_raw_to_bronze/test_clean.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import logging
 from os import listdir
 from os.path import isfile, join
-from re import search
+
 
 def raw_files_to_df(dir_name: str) -> dict[str,list]:
     """
