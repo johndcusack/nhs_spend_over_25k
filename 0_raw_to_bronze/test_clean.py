03be94deb6eb4ce662021d02158e11fc5dad87b0
johndcusack <mrjdcusack@gmail.com>
2026-07-07 15:54:46 +0100

    bug fixes

--- 0_raw_to_bronze/test_clean.py
+++ 0_raw_to_bronze/test_clean.py
@@ -27,30 +27,30 @@ def raw_files_to_df(dir_name: str) -> dict[str,list]:
     """
 
     #Note to self, this is probably going to need additional params to allow for thinks like skipping lines
-    #And/or dealing with merged cells
-    
+    #And/or dealing with merged cell
 
     def read_with_logging(file_path: str) -> pd.DataFrame|None:
-        logging.info("processing: %s",file_path)
+        logger.info("processing: %s",file_path)
         try:
             if file_path.endswith("xlsx"):
                 file_df = pd.read_excel(file_path)
             elif file_path.endswith("csv"):
                 file_df = pd.read_csv(file_path)
             else:
-                raise ValueError("file: %s not of an accepted type", file_path)
+                raise ValueError(f"file: {file_path} not of an accepted type")
         except Exception:
-            logger.exception("load error in %s", file_path)                    
+            logger.exception("load error in %s", file_path)
+            return None                    
         else:
             logger.info("success: %s", file_path)
             return file_df
 
     raw_dir: str = "/home/john/projects/spend_over_25/downloaded_data/"
-    full_dir: str = raw_dir+dir_name
+    full_dir: str = join(raw_dir,dir_name)
 
-    if dir_name not in {d for d in listdir(raw_dir)}:
+    if dir_name not in listdir(raw_dir):
         raise ValueError(f"Value error: {dir_name} not found in raw data directory")
     else:
         file_list: list = [f for f in listdir(full_dir) if isfile(join(full_dir, f)) and f.endswith(("xlsx","csv"))]
         df_list: list = [df for f in file_list if (df := read_with_logging(join(full_dir,f))) is not None]
-        return {(f"df_list-,{dir_name}"): df_list}
+        return {(f"{dir_name}"): df_list}
