82e736de7883f8c2c608634627863907b3da8c9e
johndcusack <mrjdcusack@gmail.com>
2026-08-16 12:28:28 +0100

    refactors names to prestaging

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 #import logging
 
 #from os import listdir
-from raw_to_bronze.bronze_loader import raw_files_to_df
-from raw_to_bronze.bronze_config import DOWNLOAD_DIR
+from prestaging.prestaging_extractor import raw_files_to_df
+from prestaging.prestaging_config import DOWNLOAD_DIR
 
 #file_dirs: list = listdir(DOWNLOAD_DIR)
 
---
+++ prestaging/prestaging_load_prep.py
@@ -0,0 +1,12 @@
+#plan
+import pandas as pd
+# rework config to have a column order for sorting each org's columns
+# 
+def normalise_df(df: pd.DataFrame, col_order: list):
+    pass
+
+def hash_df(df: pd.DataFrame):
+    pass
+
+def metadata_append():
+    pass
